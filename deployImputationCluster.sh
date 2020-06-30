5768440f232dc105535c866028390509c02ad1fa
Paul Underwood <paulu@amazon.com>
2020-06-30 13:05:05 -0500

    Updating readme and deployment guide.

--- deployImputationCluster.sh
+++ deployImputationCluster.sh
@@ -1,14 +1,16 @@
+clusterBucketName="REPLACE_WITH_UNIQUE_BUCKET_NAME"
+nameTagForCluster="REPLACE_WITH_TAG_YOU_WANT_TO_GIVE_CLUSTER"
+
+sed -i s/michigan-imputation-aws-public/$clusterBucketName/g imputationserver-aws/bootstrap.sh
+aws s3 sync imputationserver-aws/ s3://$clusterBucketName/
+
 npm run build
 cdk bootstrap
-clusterBucketName="devspacepaulimputationeast"
-nameTagForCluster="PaulU"
-sed -i s/michigan-imputation-aws-public/$clusterBucketName/g imputationserver-aws/bootstrap.sh
-aws s3 mb s3://$clusterBucketName
-aws s3 sync imputationserver-aws/ s3://$clusterBucketName/
 cdk deploy --context nameTag=$nameTagForCluster --context clusterBucketName=$clusterBucketName
+
 clusterId="$(aws ssm get-parameter --name EmrCluster-$nameTagForCluster --query "Parameter.Value" --output text)"
 masterNodeInstanceId="$(aws emr list-instances --cluster-id $clusterId --instance-fleet-type MASTER --query "Instances[0].Ec2InstanceId" --output text)"
 
 aws ssm start-session --target $masterNodeInstanceId \
-    document-name AWS-StartPortForwardingSession \
-    parameters '{"portNumber":["8082"],"localPortNumber":["8080"]}'
+    --document-name AWS-StartPortForwardingSession \
+    --parameters '{"portNumber":["8082"],"localPortNumber":["8080"]}'
